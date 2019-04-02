b2d7bea541ce09e1f608f30204be810ba2be848e
Charles <charles.i.saidu@gmail.com>
2019-04-02 17:08:05 +0300

    results from experiments

--- experiment.py
+++ experiment.py
@@ -19,6 +19,9 @@ from utilities import *
 
 import argparse
 
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 start_time = time.time()
 parser = argparse.ArgumentParser()
--- run_script.sh
+++ run_script.sh
@@ -7,11 +7,11 @@ echo "Starting script"
 
 # source ~/charles/environments/keras0/bin/activate
 echo 'fp_qbdc_KL'
-python  experiment.py 1 1 0
-python  experiment.py 1 1 1
+# python  experiment.py 1 1 0
+# python  experiment.py 1 1 1
 
-python  experiment.py 1 2 0
-python  experiment.py 1 2 1
+# python  experiment.py 1 2 0
+# python  experiment.py 1 2 1
 
 python  experiment.py 1 3 0
 python  experiment.py 1 3 1
@@ -38,22 +38,22 @@ python  experiment.py 3 3 0
 python  experiment.py 3 3 1
 
 
-echo 'bald'
-python  experiment.py 4 1 0
-python  experiment.py 4 1 1
+#echo 'bald'
+# python  experiment.py 4 1 0
+# python  experiment.py 4 1 1
 
-python  experiment.py 4 2 0
-python  experiment.py 4 2 1
+# python  experiment.py 4 2 0
+# python  experiment.py 4 2 1
 
-python  experiment.py 4 3 0
-python  experiment.py 4 3 1
+# python  experiment.py 4 3 0
+# python  experiment.py 4 3 1
 
-echo 'Random'
-python  experiment.py 5 1 0
-python  experiment.py 5 1 1
+# echo 'Random'
+# python  experiment.py 5 1 0
+# python  experiment.py 5 1 1
 
-python  experiment.py 5 2 0
-python  experiment.py 5 2 1
+# python  experiment.py 5 2 0
+# python  experiment.py 5 2 1
 
-python  experiment.py 5 3 0
-python  experiment.py 5 3 1
+# python  experiment.py 5 3 0
+# python  experiment.py 5 3 1
